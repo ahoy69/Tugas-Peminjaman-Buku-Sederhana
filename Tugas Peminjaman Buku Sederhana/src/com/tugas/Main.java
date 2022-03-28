f8f1167c4f4a1b13b37c53e518f675628df68a99
ahoy69 <95904044+ahoy69@users.noreply.github.com>
2022-03-28 13:27:19 +0700

    Add files via upload

---
+++ Tugas Peminjaman Buku Sederhana/src/com/tugas/Main.java
@@ -0,0 +1,67 @@
+package com.tugas;
+
+import java.util.Scanner;
+
+class Perpustakaan{
+    String nama;
+    String buku;
+    String penerbit;
+
+    Perpustakaan(String nama,String buku,String penerbit){
+        this.nama=nama;
+        this.buku=buku;
+        this.penerbit=penerbit;
+    }
+
+    void tampil(){
+        System.out.println("Nama Peminjam : "+this.nama);
+        System.out.println("Nama Buku     : "+this.buku);
+        System.out.println("Penerbit Buku : "+this.penerbit);
+        }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String nama,buku,penerbit;
+        int a,z=0,menu=0;
+        Perpustakaan[] perpus=new Perpustakaan[1024];
+        try (Scanner getstr = new Scanner(System.in)) {
+            try (Scanner getint = new Scanner(System.in)) {
+                while(menu!=3){
+                    System.out.print("Menu...\n1.Input\n2.Output\n3.Exit\nPilihan : ");
+                    menu=getint.nextInt();
+                    if(menu==1){
+                        z++;
+                        System.out.println("Masukkan data...");
+                        System.out.print("Nama Peminjam : ");
+                        nama=getstr.nextLine();
+                        System.out.print("Nama Buku     : ");
+                        buku=getstr.nextLine();
+                        System.out.print("Penerbit Buku : ");
+                        penerbit=getstr.nextLine();
+                        perpus[z]=new Perpustakaan(nama,buku,penerbit);
+                    }
+                    else if(menu==2){
+                        if(z<1){
+                            System.out.println("Data Masih Kosong...");
+                        }
+                        else{
+                            a=0;
+                            while(a<z){
+                                a++;
+                                System.out.println("---"+a+"---");
+                                perpus[a].tampil();
+                            }
+                        }
+                    }
+                    else if(menu==3){
+                        System.out.println("Keluar...");
+                    }
+                    else{
+                        System.out.println("Masukkan Menu Yang Tersedia...");
+                    }
+                }
+            }
+        }
+    }
+}
